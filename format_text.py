ecbb95b631915d3e4dbbb88192843f6142627920
mengyd <yidongmeng1992@gmail.com>
2020-05-05 04:03:21 +0200

    v2: add blending texts function

--- format_text.py
+++ format_text.py
@@ -1,21 +1,28 @@
 import os
 import re
+import random
 
 __software__ = "strip_text"
 __author__ = "MENG Yidong"
-__version__ = "1.0"
+__version__ = "2.0"
 
 # minimum words 最少词数
 __min_words__ = 4
 # maximum caracters 最大字符数
 __max_caracters__ = 110
+# choice for quiting
+__quit_choice__ = "0"
+# choice for formating
+__format_choice__ = "1"
+# choice for blending
+__blend_choice__ = "2"
 
-def format_text_file(filename: str = None):
+def format_text_by_file(filename: str = None):
     """
-    Delete the top, bottom spaces and blank lines
+    Format text in a certain way
     """
 
-    linecounter = 0
+    linecounter = 0 
     newstring = ""
     appearedlines = []
     # open original file
@@ -72,26 +79,116 @@ def repalce_file(filename: str = None):
     os.remove(filename)
     os.replace(filename+".bak", filename)
 
+def extract_text_in_file(filename: str = None):
+    """
+    read and return contents of a file
+    """
+    # open file
+    f = open(filename, 'r', encoding='UTF-8')
+    # read file
+    lines_in_file = f.readlines()
+    # close the files
+    f.close()
+    # rename file to .bak
+    os.replace(filename, filename+".bak")
+    print("extracted", len(lines_in_file), "lines in", filename)
+    return lines_in_file
+
+def write_text_into_file(filename: str = None, lines: [] = None):
+    """
+    Write every 1000 lines of contents in the list into a file
+    """
+    # Create and open a file for writing
+    f = open(filename, 'w', encoding='UTF-8')
+    # if we have enough texts
+    if len(lines) > 1000:
+        # write 1000 lines into the file
+        f.writelines(lines[:1000])
+        print("writing in", filename, ", 1000 lines.")
+        # delete these 1000 lines from the list
+        del(lines[:1000])
+    # if we don't have 1000 texts
+    elif len(lines) > 0:
+        # write all the rest into the file
+        f.writelines(lines)
+        print("writing in", filename, ",", len(lines), "lines")
+        # empty the list
+        lines.clear()
+    # close file
+    f.close()
+    # TODO: delete .bak files
+
+    return lines
+
+def write_all_in_one_file(lines: [] = None):
+    """
+    write all lines into rest.txt
+    """
+    frest = open("rest.txt", 'w', encoding='UTF-8')
+    frest.writelines(lines[:])
+    print("+" * 30)
+    print(len(lines), "lines left, writing into rest.txt")
+    print("+" * 30, "\n")
+    frest.close()
+
+''' ----------------------------Main----------------------------- '''
 # Informations
 print(__software__,  "v"+__version__)
 print("Author :", __author__)
 print('-' * 40)
 
-# get current working directory
-workpath = os.getcwd()
-print("Working in:", workpath)
-print('-' * 40)
-lines = 0
+# Choice for operations
+module_choice = __format_choice__
 
-# parse the working directory
-with os.scandir(workpath) as filelist:
-    for txtfile in filelist:
-        # do the operations for all the txt files
-        if txtfile.name.endswith(".txt"):
-            linenumber = format_text_file(filename=txtfile.name)
-            repalce_file(filename=txtfile.name)
-            print(txtfile.name, "treated; ", linenumber, "lines")
-            print("-" * 40)
+while True:
+    # get current working directory
+    workpath = os.getcwd()
+    print('<' * 40)
+    print("Working in:", workpath)
+    print('>' * 40)
+
+    # ask for module to use
+    module_choice = input("Please choose a operation( " + \
+                        __format_choice__ + " for formating, " + \
+                        __blend_choice__ + " for blending, " + \
+                        __quit_choice__ + " for quiting. ) : ")
+    
+    if module_choice == __quit_choice__:
+        print("quit...")
+        break
+
+    # parse the working directory
+    with os.scandir(workpath) as filelist:
+        if module_choice == __format_choice__: # format texts
+            for txtfile in filelist:
+                # do the formating for all the txt files
+                if txtfile.name.endswith(".txt"):
+                    linenumber = format_text_by_file(filename=txtfile.name)
+                    repalce_file(filename=txtfile.name)
+                    print(txtfile.name, "treated; ", linenumber, "lines")
+                    print("-" * 40)
+        elif module_choice == __blend_choice__: # blend texts
+            lines_in_folder = []
+            filenames_in_folder = []
+            for txtfile in filelist:
+                # do the extraction for all the txt files
+                if txtfile.name.endswith(".txt"):
+                    print("reading", txtfile.name)
+                    lines_in_folder.extend(extract_text_in_file(filename=txtfile.name))
+                    filenames_in_folder.append(txtfile.name)
+            print("extracted", len(lines_in_folder), "lines")
+            # disorganize randomly the lines
+            random.shuffle(lines_in_folder)
+            # parse the folder
+            for file_name in filenames_in_folder:
+                lines_in_folder = write_text_into_file(filename=file_name, lines=lines_in_folder)
+            # if we have more lines left
+            if len(lines_in_folder) > 0:
+                # write them all into rest.txt
+                write_all_in_one_file(lines=lines_in_folder)
+            
+        print("-" * 40)
+
 
 # pause
 os.system('pause')
--- readme.md
+++ readme.md
@@ -13,10 +13,18 @@
 * Delete the lines **too long**
 * Delete the lines that have **numbers**
 * Display the number of **valid** lines of each file
+* Disorganize **randomly** all the texts in the .txt files in a folder
 
 ### Use guide
 Put the ``format_text.py`` file into the folder with .txt files to format, then just **run** the script.
+
 #### Options
+##### When the program ask you to choose a operation: 
+0. Tap "0" for **quiting** the program
+1. Tap "1" for **formating** the .txt files
+2. Tap "2" for **blending** all the lines in the .txt files in a folder 
+
+### Constants
 Change the minimum words and maximum caracters in a line as we want : 
 * Minimum words : 
   * Change the value here at the top of the "format_text.py" file : ``__min_words__ = 4``
@@ -39,11 +47,18 @@ ___________
 * 删除**过长**的句子
 * 删除**带数字**的句子
 * 显示**合格**行数
+* **随机**打乱一个文件夹中所有txt文件中的句子，并将打乱后的句子重新分配至不同文件中
 
 ### 用法
 把 ``format_text.py`` 文件放入含有待审核的txt文档的文件夹内, 然后**运行**该脚本就行了。
 
-#### 可选
+#### 选项
+##### 当程序要求你输入执行选项时: 
+0. 输入 "0" 以**退出**程序
+1. 输入 "1" 以执行**标准化**txt文档
+2. 输入 "2" 以执行**打乱** 文件夹中所有txt文档内的句子
+
+#### 常量
 可根据需要改变要求的最小单词数和最大字符数 : 
 * 调整**最小单词数** : 
   * 在 "format_text.py" 文件顶部改变这里的数值 : <br>
