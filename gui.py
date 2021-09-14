a69e1867e116d69d5ec3b9ea9ad715e5d8402f6b
Yidong <yidongmeng1992@gmail.com>
2021-09-14 22:06:09 +0200

    Add Options

--- gui.py
+++ gui.py
@@ -16,13 +16,32 @@ def modulesListSetup():
         lst.insert('end', module)
     return lst
 
+def onClick_formatOptions(var):
+    print(var.get())
+
+def formatTextOptionsSetup(window=None):
+    var = tk.StringVar()
+    ops1 = tk.Radiobutton(window, text='标准化', variable=var, value='1', command=onClick_formatOptions(var))
+    ops2 = tk.Radiobutton(window, text='打乱', variable=var, value='2', command=onClick_formatOptions(var))
+    ops3 = tk.Radiobutton(window, text='删除备份', variable=var, value='3', command=onClick_formatOptions(var))
+    ops1.pack()
+    ops2.pack()
+    ops3.pack()
+    return var
+
 def formatTextPageSetup(workpath):
     textFormatPage = tk.Toplevel(homepage)
     textFormatPage.geometry('1000x800')
     textFormatPage.title('Text Format')
+
     var_lable = tk.StringVar(textFormatPage, value='Working on: ' + workpath)
     lable_workpath = tk.Label(textFormatPage, bg='green', fg='yellow',font=('Arial', 12), width=800, textvariable=var_lable)
     lable_workpath.pack()
+
+    var = formatTextOptionsSetup(window=textFormatPage)
+
+    print(var.get(), "...")
+    
     return textFormatPage
 
 def processExcelPageSetup(workpath):
@@ -52,9 +71,9 @@ def onClick_btn_selectMods():
     dir_path = entry_chooseFile.get()
     print(dir_path)
     if value == '文本处理':
-        textFormatPage = formatTextPageSetup(dir_path)
+        formatTextPageSetup(dir_path)
     elif value == 'Excel处理':
-        processExcelPage = processExcelPageSetup(dir_path)
+        processExcelPageSetup(dir_path)
 
 def selectBtnSetup():
     btn = tk.Button(homepage, text="选择", font=('Arial', 12), width=10, height=1, command=onClick_btn_selectMods)
